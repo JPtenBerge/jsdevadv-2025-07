a4de09e9f69421c6f9151f547528d44e41a106bf
JP <jp@jp.nl>
2025-07-31 15:35:32 +0200

    Sets / maps

---
+++ sets-and-maps.js
@@ -0,0 +1,44 @@
+
+
+let setje = new Set();
+setje.add(123);
+setje.add(123);
+setje.add(123);
+setje.add('123'); // ===
+setje.add({});
+setje.add({});
+console.log(setje.size);
+setje.clear(); // ipv .length = 0
+// console.log(setje.values());
+
+console.log(setje);
+
+let func = () => {};
+
+let mapje = new Map();
+mapje.set('qwer', 123);
+mapje.set('qwer', () => {});
+mapje.set({}, {});
+mapje.set(func, []);
+
+let obj = {
+    qwert: 1234
+};
+obj['❤🎂😁🤞'] = 123;
+obj[{}] = 123;
+console.log(obj);
+
+// mapje.
+
+// Map vs {}
+// - codedocent Map
+// - .size
+// - behoudt volgorde
+// - complexe keys
+
+console.log(mapje);
+
+
+// WeakMap
+// WeakSet
+
